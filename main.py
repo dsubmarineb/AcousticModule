c6c4a52770324e474a529f7a89b2cfbf76989c84
dsubmarineb <genechoi@utexas.edu>
2021-10-05 23:34:07 -0500

    psudo content update

--- main.py
+++ main.py
@@ -4,16 +4,13 @@ print("hi")
 
 '''
 
-
-
-
-
-
-
-
-
-
-
+1. prompt user for input or select audio input mode (assume input is .wav file)
+2. read in audio file
+3. plot audio file in matplotlib
+    3-1. plot amplitude vs time
+    3-2. plot amplitude vs frequency (Fourier transform)
+    * Q. How can I represent change in Fourier Transform over time?
+4. ???
 
 
 
