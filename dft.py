dda14cf8ab8e41d4adb69e1593ee4b07b50404a1
dsubmarineb <genechoi@utexas.edu>
2021-11-09 22:42:04 -0600

    Tried doing dft by looping over k and n (freq and time) but too slow

--- dft.py
+++ dft.py
@@ -1,5 +1,67 @@
 # Implement DFT Transformation
+import numpy as np
+import math
+import wave
 
-def dft():
 
-    return
+# Separate data into left and right channels
+def readData(data):
+    left, right = [], []
+    for i in range(0,len(data),2):
+        snip = data[i:i+2]
+        snip = int.from_bytes(snip, byteorder='little')
+    
+        if snip > pow(2, 15):
+            snip -= pow(2, 16)
+        if i%4 == 0:
+            left.append(snip)
+        else:
+            right.append(snip)
+    return (left,right)
+
+
+def dft(fileName):
+
+    # Redundant code from drawAudio:
+    # - get separate array of left and right chennel data   
+                                                   
+    # Open wave file
+    wf = wave.open(fileName, 'rb')
+
+    # Get variables
+    channels    = wf.getnchannels()
+    samplewidth = wf.getsampwidth()
+    samplerate  = wf.getframerate()
+
+    # Set data chunk size - amount to be handled each read
+    frame_count = 1024
+
+    # Read in data and separate into 2 channels
+    data = wf.readframes(frame_count)
+    left, right = ([], [])
+    dataSize = frame_count * samplewidth * channels
+
+    while (len(data) == dataSize): # loop until end of file
+
+        lr = readData(data) 
+        left.extend(lr[0])
+        right.extend(lr[1])
+        data = wf.readframes(frame_count)
+    
+    x_n = left
+    PI = math.pi
+    e = math.e
+    i = 1j
+    N = len(x_n)
+    print(N)
+
+    x_k = [0] * len(x_n)
+    for k in range(len(x_k)):
+        x_k[k] = sum([(x_n[n] * math.cos(2*PI*k*n/N)) for n in range(N)])
+        print(x_k[k])
+    x_t = left
+
+    x_k = x_t * []
+    print('hi')
+    
+dft("whistle.wav")
--- drawAudio.py
+++ drawAudio.py
@@ -2,9 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import wave
 
+# 11/08/21 - assumes wave file is 2-channel and 16-bit PCM
+
+# Separate data into left and right channels
 def readData(data):
-    left = []
-    right = []
+    left, right = [], []
     for i in range(0,len(data),2):
         snip = data[i:i+2]
         snip = int.from_bytes(snip, byteorder='little')
@@ -17,37 +19,51 @@ def readData(data):
             right.append(snip)
     return (left,right)
 
+# Store data in an array and plot over time
 def draw(fileName):
+
+    # Open wave file
     wf = wave.open(fileName, 'rb')
 
-    channels = wf.getnchannels()
+    # Get variables
+    channels    = wf.getnchannels()
     samplewidth = wf.getsampwidth()
-    samplerate = wf.getframerate()
-    frames = wf.getnframes()
+    samplerate  = wf.getframerate()
 
+    # Set data chunk size - amount to be handled each read
     frame_count = 1024
 
+    # Read in data and separate into 2 channels
     data = wf.readframes(frame_count)
     left, right = ([], [])
-    while (len(data)==frame_count*samplewidth*channels):
+    dataSize = frame_count * samplewidth * channels
+
+    while (len(data) == dataSize): # loop until end of file
 
         lr = readData(data) 
         left.extend(lr[0])
         right.extend(lr[1])
         data = wf.readframes(frame_count)
-        # print(len(data))
 
+    # Calculate variables
+    frames = len(left)
+    duration = frames / samplerate
+    sampleperiod = 1 / samplerate
+    
+    # Plot result
     fig, ax = plt.subplots()
-
-    ax.plot(left, label='Left Channel', linewidth=0.1)
-    ax.plot(right, label='Right Channel', linewidth=0.1)
-    ax.set(xlabel='Frames', ylabel='Amplitude', 
+    plotrange = np.arange(0, duration, sampleperiod)
+    ax.plot(plotrange, left, label='Left Channel')
+    ax.plot(plotrange, right, label='Right Channel')
+    ax.set(xlabel='Time (s)', ylabel='Amplitude', 
             title='Amplitude vs Time')
     ax.legend()
 
     plt.show()
 
+    # Close wave file
     wf.close()
     return
 
+# For debugging:
 # draw("whistle.wav")
