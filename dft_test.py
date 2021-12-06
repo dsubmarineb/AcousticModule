1fb76071f94dd6b3cab4c76a915fd9a677685a5e
dsubmarineb <genechoi@utexas.edu>
2021-12-06 10:29:39 -0600

    fft plots and step size change

--- dft_test.py
+++ dft_test.py
@@ -4,13 +4,21 @@ import matplotlib.pyplot as plt
 n = 5
 t= np.arange(0, n, 0.01)
 f_t = np.sin(2*np.pi*t)
+f_t2 = np.sin(1.5*2*np.pi*t)
 
+# applying from fftfreq definition
+n = f_t.size
+timestep = 0.01
 sp = np.fft.fft(f_t)
-freq = np.fft.fftfreq(t.shape[-1])*100
+freq = np.fft.fftfreq(t.shape[-1], d=0.01)
+
+sp2 = np.fft.fft(f_t2)
 
 fig, axs = plt.subplots(2)
 axs[0].plot(t, f_t)
+axs[0].plot(t, f_t2)
 axs[1].plot(freq, abs(sp))
+axs[1].plot(freq, abs(sp2))
 
 plt.show()
 
